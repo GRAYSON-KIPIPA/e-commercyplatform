12d9cf3be98aa4ececf84414acdfee29ec85af6a
grayson-kipipa <mapundagrayson@gmail.com>
2024-08-23 00:45:32 +0300

    First commit done in 22 August 2024

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import Login from "./pages/Login";
+
+function App() {
+  return (
+    <>
+      <Login />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Login.tsx
@@ -0,0 +1,54 @@
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import { Button } from "@mui/material";
+import ArrowRightAltIcon from "@mui/icons-material/ArrowRightAlt";
+const Login = () => {
+  return (
+    <div className="grid h-48 place-items-center border-1 mt-36">
+      <Box
+        sx={{ width: 500 }}
+        className="flex justify-center  grid grid-rows gap-4"
+      >
+        <div className="gap-1">
+          <Typography className="font-semibold">Login to ShopKonect</Typography>
+          <Typography className="text-sm font-light">
+            Please enter your email and password to sign into the system
+          </Typography>
+        </div>
+        <div>
+          <Typography className="font-semibold">Email</Typography>
+          <TextField
+            type="email"
+            id="outlined-basic"
+            label="example@mail.com"
+            variant="outlined"
+            size="small"
+            className="w-96 rounded-3xl"
+          />
+        </div>
+        <div>
+          <div className="flex gap-64">
+            <Typography className="font-semibold">Password</Typography>
+            <Typography className="font-normal">
+              <a href="#">forgot</a>
+            </Typography>
+          </div>
+          <TextField
+            id="outlined-basic"
+            label="your password"
+            variant="outlined"
+            size="small"
+            className="w-96 rounded-full"
+          />
+        </div>
+        <Button className="w-96" variant="contained">
+          Login
+          <ArrowRightAltIcon />
+        </Button>
+      </Box>
+    </div>
+  );
+};
+
+export default Login;
